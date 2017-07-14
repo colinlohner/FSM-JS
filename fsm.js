9abe7a50c2b2cd10d1f6437fa99c7886653d8ef1
colinlohner <colin@colinlohner.com>
2017-07-13 22:53:28 -0500

    First version of FSM.js

---
+++ fsm.js
@@ -0,0 +1,73 @@
+//Setup
+var fsmActual = document.createElement('div');
+fsmActual.setAttribute('id', 'fsm_actual');
+document.body.appendChild(fsmActual);
+var $fsm = document.querySelectorAll('.fsm');;
+var $fsmActual = document.querySelector('#fsm_actual');
+$fsmActual.style.position = "absolute";
+
+var position = {};
+var size = {};
+
+
+//modal action stuffs
+var openFSM = function(event) {
+	var $this = event.currentTarget;
+	position = $this.getBoundingClientRect();
+	size = {
+		width: window.getComputedStyle($this).width,
+		height: window.getComputedStyle($this).height
+	}
+	
+	$fsmActual.style.position = "absolute";
+	$fsmActual.style.top = position.top + 'px';
+	$fsmActual.style.left = position.left + 'px';
+	$fsmActual.style.height = size.height;
+	$fsmActual.style.width = size.width;
+	$fsmActual.style.margin = $this.style.margin;
+	
+	setTimeout(function(){
+		$fsmActual.innerHTML = $this.innerHTML;
+		var classes = $this.classList.value.split(' ');
+		for (var i = 0; i < classes.length; i++) {
+			$fsmActual.classList.add(classes[i]);
+		}
+		$fsmActual.classList.add('growing');
+		$fsmActual.style.height = '100vh';
+		$fsmActual.style.width = '100vw';
+		$fsmActual.style.top = '0';
+		$fsmActual.style.left = '0';
+		$fsmActual.style.margin = '0';
+	}, 1);
+	
+	setTimeout(function(){
+		$fsmActual.classList.remove('growing');
+		$fsmActual.classList.add('full-screen')
+	}, 1000);
+};
+
+var closeFSM = function(event){
+	var $this = event.currentTarget;
+	
+	$this.style.height = size.height;
+	$this.style.width = size.width;
+	$this.style.top = position.top + 'px';
+	$this.style.left = position.left + 'px';
+	$this.style.margin = '0';
+	$this.classList.remove('full-screen');
+	$this.classList.add('shrinking');
+	
+	setTimeout(function(){
+		while($this.firstChild) $this.removeChild($this.firstChild);
+		var classList = $this.classList;
+		while (classList.length > 0) {
+			 classList.remove(classList.item(0));
+		}
+		$this.style = '';;
+	}, 1000);
+};
+
+for (var i = 0; i < $fsm.length; i++) {
+	$fsm[i].addEventListener("click", openFSM);
+}
+$fsmActual.addEventListener("click", closeFSM);
